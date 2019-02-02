c43f523993c4d3aa733f74ee08495f82487793a0
Giora Guttsait <giora111@gmail.com>
2019-02-02 16:16:58 +0200

    Got base code for stopwatch

---
+++ src/app/stopwatch.service.spec.ts
@@ -0,0 +1,115 @@
+import { TestBed } from '@angular/core/testing';
+
+import { StopwatchService, Stopwatch } from './stopwatch.service';
+
+describe('Stopwatch', () => {
+  let workout: Stopwatch;
+  let startTime: number;
+
+  beforeEach(() => {
+    startTime = Date.now();
+    workout = new Stopwatch(startTime);
+  });
+
+  describe('Creation', () => {
+    it('should be created with given `startTime` and no laps', () => {
+      expect(workout.startTime).toBe(startTime);
+      expect(workout.laps).not.toBeFalsy();
+      expect(workout.laps.length).toBe(0);
+    });
+  });
+
+  describe('Starting New Lap', () => {
+    it('should add a lap given valid time', () => {
+      const LAP_DURATION = 5;
+      workout.startNewLap(startTime + LAP_DURATION);
+      expect(workout.laps.length).toBe(1);
+      expect(workout.laps[0]).toBe(5);
+    });
+
+    it('should add multiple laps given valid time', () => {
+      const FIRST_LAP_DURATION = 5;
+      const SECOND_LAP_DURATION = 7;
+      workout.startNewLap(startTime + FIRST_LAP_DURATION);
+      workout.startNewLap(startTime + FIRST_LAP_DURATION + SECOND_LAP_DURATION);
+      expect(workout.laps.length).toBe(2);
+      expect(workout.laps[1]).toBe(SECOND_LAP_DURATION);
+    });
+
+    it('should not add a lap given invalid first lap time', () => {
+      expect(() => workout.startNewLap(startTime - 1)).toThrow();
+    });
+
+    it('should not add a lap given invalid non-first lap time', () => {
+      workout.startNewLap(startTime + 5);
+      expect(() => workout.startNewLap(startTime + 2)).toThrow();
+    });
+  });
+
+  describe('Elapsed Time and Current Lap Time', () => {
+    it('should return elapsedTime', () => {
+      const TIME_SINCE_START = 5;
+      workout.elapsedTime(startTime + TIME_SINCE_START);
+      expect(workout.elapsedTime(startTime + TIME_SINCE_START)).toBe(TIME_SINCE_START);
+    });
+
+    it('should return elapsedTime after laps', () => {
+      const LAP_DURATION = 5;
+      const TOTAL_LAPS = 4;
+      for (let i = 1; i <= TOTAL_LAPS; i++) {
+        workout.startNewLap(startTime + LAP_DURATION * i);
+      }
+
+      const EXPECTED_DURATION = TOTAL_LAPS * LAP_DURATION;
+      expect(workout.elapsedTime(startTime + EXPECTED_DURATION)).toBe(EXPECTED_DURATION);
+    });
+
+    it('should return elapsed time on currentLapTime before laps are recorded', () => {
+      const ELAPSED_TIME = 5;
+      expect(workout.currentLapTime(startTime + ELAPSED_TIME)).toBe(ELAPSED_TIME);
+    });
+
+    it('should return only the time of the current lap after laps are recorded', () => {
+      const ELAPSED_TIME = 12;
+      const LAP_DURATION = 5;
+      workout.startNewLap(startTime + LAP_DURATION);
+      expect(workout.currentLapTime(startTime + ELAPSED_TIME)).toBe(ELAPSED_TIME - LAP_DURATION);
+    });
+  });
+
+  describe('Pausing and Resuming', () => {
+    it('should not add laps after pausing', () => {
+      const ELAPSED_TIME_UNTIL_PAUSE = 5;
+      workout.pause(startTime + ELAPSED_TIME_UNTIL_PAUSE);
+      workout.startNewLap(0);
+      workout.startNewLap(Number.POSITIVE_INFINITY);
+      expect(workout.laps.length).toBe(0);
+    });
+
+    it('should return elapsedTime based on pauseTime regardless of provided time', () => {
+      const ELAPSED_TIME_UNTIL_PAUSE = 5;
+      workout.pause(startTime + ELAPSED_TIME_UNTIL_PAUSE);
+      expect(workout.elapsedTime(0)).toBe(ELAPSED_TIME_UNTIL_PAUSE);
+      expect(workout.elapsedTime(Number.POSITIVE_INFINITY)).toBe(ELAPSED_TIME_UNTIL_PAUSE);
+    });
+
+    it('should return currentLapTime based on pauseTime regardless of provided time', () => {
+      const ELAPSED_TIME_UNTIL_PAUSE = 20;
+      const LAP_DURATION = 5;
+      workout.startNewLap(startTime + LAP_DURATION);
+      workout.pause(startTime + ELAPSED_TIME_UNTIL_PAUSE);
+      const EXPECTED_DURATION = ELAPSED_TIME_UNTIL_PAUSE - LAP_DURATION;
+      expect(workout.currentLapTime(0)).toBe(EXPECTED_DURATION);
+      expect(workout.currentLapTime(Number.POSITIVE_INFINITY)).toBe(EXPECTED_DURATION);
+    });
+  });
+});
+
+describe('WorkoutTimerService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should be created', () => {
+    const service: StopwatchService = TestBed.get(StopwatchService);
+    expect(service).toBeTruthy();
+  });
+});
---
+++ src/app/stopwatch.service.ts
@@ -0,0 +1,153 @@
+import { Injectable } from '@angular/core';
+import { interval, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+export enum TimerState {
+  Stopped = 'stopped',
+  Running = 'running',
+  Paused = 'paused',
+}
+
+export class Stopwatch {
+  currentLapBeginning: number;
+  pauseTime: number | null = null;
+
+  static startNew(startTime?: number): Stopwatch {
+    startTime = startTime || Date.now();
+    return new Stopwatch(Date.now());
+  }
+
+  constructor(public startTime: number, public laps: number[] = []) {
+    this.currentLapBeginning = startTime;
+  }
+
+  private get paused(): boolean {
+    return this.pauseTime !== null;
+  }
+
+  startNewLap(beginningOfNewLap: number): void {
+    if (this.paused) {
+      return;
+    }
+
+    const currentLapDuration = beginningOfNewLap - this.currentLapBeginning;
+    if (currentLapDuration < 0) {
+      throw new Error(`Lap ${this.laps.length + 1} recorded with invalid time: ${beginningOfNewLap} is before ${this.currentLapBeginning}`);
+    }
+    this.laps.push(currentLapDuration);
+    this.currentLapBeginning = beginningOfNewLap;
+  }
+
+  pause(pauseTime: number): void {
+    if (this.paused) {
+      return;
+    }
+    this.pauseTime = pauseTime;
+  }
+
+  resume(resumeTime: number): void {
+    if (!this.paused) {
+      return;
+    }
+
+    const timeUntilResume = resumeTime - this.pauseTime;
+    if (timeUntilResume < 0) {
+      throw new Error(`Attempt to resume to time(${resumeTime}) before pause time(${this.pauseTime})`);
+    }
+
+    this.pauseTime = null;
+    this.startTime += timeUntilResume;
+    this.currentLapBeginning += timeUntilResume;
+  }
+
+  currentLapTime(nowTime: number) {
+    if (this.paused) {
+      return this.pauseTime - this.currentLapBeginning;
+    }
+    return nowTime - this.currentLapBeginning;
+  }
+
+  elapsedTime(nowTime: number) {
+    if (this.paused) {
+      return this.pauseTime - this.startTime;
+    }
+    return nowTime - this.startTime;
+  }
+}
+
+interface StopwatchState {
+  elapsedDuration: number;
+  laps: number[];
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StopwatchService {
+  timerState = TimerState.Stopped;
+  stopwatch: Stopwatch = null;
+
+  constructor() { }
+
+  public stopwatchState$(): Observable<StopwatchState | null> {
+    return interval(10).pipe(
+      map(() => {
+        if (!this.hasRecord) {
+          return null;
+        }
+        return {
+          elapsedDuration: this.elapsedDuration(),
+          laps: [this.currentLapDuration(), ...this.stopwatch.laps],
+        };
+      })
+    );
+  }
+
+  get hasRecord(): boolean {
+    return this.stopwatch !== null;
+  }
+
+  start(): void {
+    if (this.timerState === TimerState.Running) {
+      return;
+    }
+
+    this.timerState = TimerState.Running;
+    this.stopwatch = Stopwatch.startNew();
+  }
+
+  startNewLap(): void {
+    if (this.timerState === TimerState.Running) {
+      this.stopwatch.startNewLap(Date.now());
+    }
+  }
+
+  stop(): void {
+    this.timerState = TimerState.Stopped;
+    this.stopwatch.pause(Date.now());
+  }
+
+  pause(): void {
+    this.timerState = TimerState.Paused;
+    this.stopwatch.pause(Date.now());
+  }
+
+  resume(): void {
+    this.timerState = TimerState.Running;
+    this.stopwatch.resume(Date.now());
+  }
+
+  private elapsedDuration(): number {
+    if (!this.hasRecord) {
+      return 0;
+    }
+    return this.stopwatch.elapsedTime(Date.now());
+  }
+
+  private currentLapDuration(): number {
+    if (!this.hasRecord) {
+      return 0;
+    }
+    return this.stopwatch.currentLapTime(Date.now());
+  }
+}
